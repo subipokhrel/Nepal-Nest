a19fa8d24e6823e75d5d16d71536ae405f49000e
subiiiiiii <107203752+subiiiiiii@users.noreply.github.com>
2024-11-05 15:40:17 +0545

    Initial frontend setup

---
+++ client/src/App.js
@@ -0,0 +1,15 @@
+import logo from './logo.svg';
+import './App.css';
+import Navbar from './components/navbar';
+
+function App() {
+  return (
+    <div className="App">
+        <Navbar/>
+
+      <h1>Welcome to Sajilo Stay</h1>
+    </div>
+  );
+}
+
+export default App;
